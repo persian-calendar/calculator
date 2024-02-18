48932d04954b4f1b17ab81809868ea3789310c1d
Ebrahim Byagowi <ebrahim@gnu.org>
2024-02-18 13:33:57 +0330

    Upgrade Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     antlr
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.9.22"
     `maven-publish`
 }
 
@@ -39,7 +39,7 @@ tasks.test {
 }
 
 configure<JavaPluginExtension> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 publishing {
